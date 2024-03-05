091066d2922a55fe7d7330577445d3c0404a168c
Valeriu-Cristian Mandeal <mandealvaleriucristian@gmail.com>
2024-03-05 18:31:01 +0200

    Modify Customer List Dto

--- API/Dtos/CustomerForListDto.cs
+++ API/Dtos/CustomerForListDto.cs
@@ -4,5 +4,6 @@ public class CustomerForListDto
 {
     public int Id { get; set; }
     public required string Name { get; set; }
+    public required string Email { get; set; }
     public required string WebsiteURL { get; set; }
 }
